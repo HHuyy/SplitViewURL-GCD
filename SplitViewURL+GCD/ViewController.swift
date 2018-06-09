f430e1f29131e72ec68b006d23c42fd434fd357c
tham gia huy <>
2018-06-09 15:40:46 +0700

    new

---
+++ SplitViewURL+GCD/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SplitViewURL+GCD
+//
+//  Created by tham gia huy on 6/9/18.
+//  Copyright © 2018 tham gia huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var photo: Photo? {
+        didSet {
+            refreshUI()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func refreshUI() {
+        loadViewIfNeeded()
+        if let url = URL(string: (photo?.picture)!){
+            do {
+                let data = try Data(contentsOf: url)
+                self.imageView.image = UIImage(data: data)
+            }catch let error {
+                print("Error : \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
+
+extension ViewController: PhotoSelectionDelegate {
+    func PhotoSelected(_ newPhoto: Photo) {
+            photo = newPhoto
+    }
+}
